08e9b113bd7f3965e2932c9ad54288d4228fee79
kuku <kuku@kuku.me>
2021-07-21 13:23:11 +0800

    update

--- src/main/java/me/kuku/utils/DateTimeFormatterUtils.java
+++ src/main/java/me/kuku/utils/DateTimeFormatterUtils.java
@@ -1,8 +1,10 @@
 package me.kuku.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalQueries;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DateTimeFormatterUtils {
@@ -21,6 +23,18 @@ public class DateTimeFormatterUtils {
 	public static String format(long time, String pattern){
 		LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(time / 1000, 0, ZoneOffset.ofHours(8));
 		return creat(pattern).format(localDateTime);
+	}
+
+	public static long parseDateTime(String str, String pattern){
+		DateTimeFormatter dtf = creat(pattern);
+		LocalDateTime localDateTime = LocalDateTime.parse(str, dtf);
+		return localDateTime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+	}
+
+	public static long parseDate(String str, String pattern){
+		DateTimeFormatter dtf = creat(pattern);
+		LocalDate localDate = LocalDate.parse(str, dtf);
+		return localDate.atStartOfDay().toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
 	}
 
 	public static DateTimeFormatter creat(String pattern){
